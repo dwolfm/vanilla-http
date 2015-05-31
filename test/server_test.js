4d0cb29d734c01c458f9b89c2b1d5762066fec7f
Duncan Marsh <dwolfm@gmail.com>
2015-05-30 19:16:39 -0700

    wrote test for greet

--- test/server_test.js
+++ test/server_test.js
@@ -5,5 +5,29 @@ var expect = chai.expect;
 chai.use(chaihttp);
 
 describe('sever.js', function(){
-	it('should 
+	describe('/greet', function(){
+		describe('get /greet/duncan', function(){
+			it('should return whatsup duncan', function(done){
+				chai.request('localhost:3000')
+					.get('/greet/duncan')
+					.end(function(err, res){
+						if (err) throw err;
+							expect(res.text).to.eql('whatsup duncan');
+							done();
+					});
+			});
+		});
+		describe('post /greet', function(){
+			it('should return whatsup slug', function(done){
+				chai.request('localhost:3000')
+					.post('/greet')
+					.send({name:"slug"})
+					.end(function(err, res){
+						if (err) throw err;
+						expect(res.text).to.eql('whatsup slug');
+						done();
+					});
+			});
+		});
+	});
 });
